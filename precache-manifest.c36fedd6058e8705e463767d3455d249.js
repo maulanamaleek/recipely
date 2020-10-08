6c4d814641a03f8868348474fb65fe0eb9da47fe
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-10-08 23:09:10 +0700

    Updates

---
+++ precache-manifest.c36fedd6058e8705e463767d3455d249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f2159d752609f91535f7709d19153c",
+    "url": "/recipely/index.html"
+  },
+  {
+    "revision": "8010a68bdda9f7544953",
+    "url": "/recipely/static/css/main.5b923c90.chunk.css"
+  },
+  {
+    "revision": "eea9a8bea17a65e9bf30",
+    "url": "/recipely/static/js/2.3eb317ea.chunk.js"
+  },
+  {
+    "revision": "e867553593e1bcee119717999d316408",
+    "url": "/recipely/static/js/2.3eb317ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8010a68bdda9f7544953",
+    "url": "/recipely/static/js/main.8cb2218e.chunk.js"
+  },
+  {
+    "revision": "e43ccee2f34a9324e783",
+    "url": "/recipely/static/js/runtime-main.51a4e2ae.js"
+  }
+]);
